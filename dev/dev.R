cd9f73d24ef6b93e82feeb6b65c422dc3e4888c9
Januar Harianto <januar.harianto@sydney.edu.au>
2025-10-20 23:22:44 +1100

    initial commit

---
+++ dev/dev.R
@@ -0,0 +1,9 @@
+# Development script for experimenting with package functions
+# Load the package
+devtools::load_all()
+
+# Example: Test the detect_unit function
+# result <- detect_unit("path/to/test.xlsx", "your_pattern")
+# print(result)
+
+# Add your experimental code here
