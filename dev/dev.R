91a00d33a133faa9386f6a43b214ac95d0ddb015
Januar Harianto <januar.harianto@sydney.edu.au>
2025-10-21 10:26:31 +1100

    fix: remove file used for dev testing

--- dev/dev.R
+++
@@ -1,9 +0,0 @@
-# Development script for experimenting with package functions
-# Load the package
-devtools::load_all()
-
-# Example: Test the detect_unit function
-# result <- detect_unit("path/to/test.xlsx", "your_pattern")
-# print(result)
-
-# Add your experimental code here
